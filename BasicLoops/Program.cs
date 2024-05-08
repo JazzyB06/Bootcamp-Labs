761796e081bcbdb86ba0ff8dd653dd689da351d4
Jasmine Boykins <jasmine_boykins@yahoo.com>
2024-05-08 15:13:54 -0400

    I would like to commit

---
+++ BasicLoops/Program.cs
@@ -0,0 +1,15 @@
+﻿string userInput;
+
+int i = 0;
+do
+{
+    Console.WriteLine("Hello World!");
+    Console.WriteLine("Would you like to continue 'Y' for 'yes' and 'N' for 'no'.");
+    userInput = Console.ReadLine();
+    i++;
+}
+while (i<5);
+{
+    userInput = Console.ReadLine();
+    Console.WriteLine("Goodbye!");
+}
