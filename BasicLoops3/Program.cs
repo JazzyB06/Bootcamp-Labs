06e806ea6b0712ee767d18d424f80747d639a9f6
Jasmine Boykins <jasmine_boykins@yahoo.com>
2024-05-08 15:19:40 -0400

    Commit please

---
+++ BasicLoops3/Program.cs
@@ -0,0 +1,12 @@
+﻿int KeyCode = 0;
+
+while (KeyCode != 13579)
+{
+    Console.WriteLine("Kindly, enter your Key Code, please.");
+    KeyCode = int.Parse(Console.ReadLine());
+
+    if (KeyCode == 13579)
+    {
+        Console.WriteLine("It's good to be home! Hello, Jasmine!");
+    }
+}   
