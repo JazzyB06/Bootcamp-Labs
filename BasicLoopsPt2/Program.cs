887e7c1d732af4a3861df08cc3f0e7347258a37b
Jasmine Boykins <jasmine_boykins@yahoo.com>
2024-05-08 15:17:29 -0400

    Committing this file

---
+++ BasicLoopsPt2/Program.cs
@@ -0,0 +1,26 @@
+﻿for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+    
+}
+
+for (int i = 5; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Would you like to continue 'Y' or 'N'?");
+Console.ReadLine();
+
+for (int i = 0; i < 12; i++)
+{
+    Console.WriteLine(i);
+
+}
+
+for (int i = 12; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Would you like to continue 'Y' or 'N'?");
+Console.ReadLine();
+Console.WriteLine("Goodbye!");
