06011bed19a5fe2c45839b497b10fff3591d0d58
Jasmine Boykins <jasmine_boykins@yahoo.com>
2024-05-08 15:22:18 -0400

    Commit please

---
+++ BasicLoops5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Metrics;
+
+int KeyCode = 0;
+int userinput = 0;
+
+while (KeyCode != 13579)
+{
+    Console.WriteLine("Kindly, enter your Key Code, please.");
+    KeyCode = int.Parse(Console.ReadLine());
+    if (KeyCode == 13579)
+    {
+        Console.WriteLine("It's good to be home! Hello, Jasmine!");
+    }
+    do
+    {
+        Console.WriteLine("Your answer is incorrect");
+        userinput = int.Parse(Console.ReadLine());
+
+        break;
+
+    }
+    while (userinput != 0);
+    {
+        Console.WriteLine("You've reached the maximum amount of attempts.");
+        Console.ReadLine();
+    }
+    while (true) ;
+}
