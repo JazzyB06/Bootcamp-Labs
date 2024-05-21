5eec2cf257c182a9b6a51037600da9e140de403a
Jasmine Boykins <jasmine_boykins@yahoo.com>
2024-05-21 18:28:47 -0400

    Please commit

--- Assessment1/Program.cs
+++ Assessment1/Program.cs
@@ -1,1 +1,43 @@
 ﻿
+
+using System.ComponentModel.Design;
+using System.Numerics;
+
+Console.WriteLine(GetDaysLeft(50, 7));
+static double GetDaysLeft(double num1, double num2)
+{
+    return num1 - num2;
+}
+
+
+Console.WriteLine($"Please enter a number:");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+if (number <= 6) ;
+{
+    Console.WriteLine("The next topic is Continents");
+    Console.WriteLine("");
+}
+while (false) ;
+
+if (number <= 20) ;
+{
+    Console.WriteLine("The next topic is Oceans");
+    Console.WriteLine("");
+}
+while (false) ;
+if (number <= 37) ;
+{
+    Console.WriteLine("The next topic is The Solar System");
+    Console.WriteLine("");
+}
+while (false) ;
+if (number <= 50) ;
+{
+    Console.WriteLine("There are no more tests left");
+    Console.WriteLine("");
+}
+Console.WriteLine($"You have {userInput} days left");
+Console.WriteLine("");
+Console.WriteLine("There are more days left of class ");
+Console.WriteLine("Please choose another day of class. Goodbye!");
---
+++ BasicStringProcessing/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string sentence = "The sheep jumped over the fence and smiled.";
+string[]words = sentence.Split(' ');
+
+foreach (string word in words)
+    Console.WriteLine(word);
+
+Console.WriteLine("Would you like to continue. Click 'y' for 'yes' or 'n' for 'no'. ");
+//if (()) ;
+//else
+    Console.WriteLine("Please enter another sentence.");
