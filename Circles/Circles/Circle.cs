41646093539ffca219b562045b52526e4dc931c1
JazzyB06 <jasmine_boykins@yahoo.com>
2024-06-05 14:00:04 -0400

    Circles

---
+++ Circles/Circles/Circle.cs
@@ -0,0 +1,36 @@
+﻿namespace Circles;
+
+public class Circle
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double CalculateDiameter()
+    {
+        return 2 * radius;
+    }
+
+    public double CalculateCircumference()
+    {
+        return 2 * Math.PI * radius;
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public void Grow(double radius)
+    {
+        radius *= radius;
+    }
+
+    public double GetRadius()
+    {
+        return radius;
+    }
+}
---
+++ Circles/Circles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Program
+{
+    public class Circle
+    {
+        private double radius;
+
+        public Circle()
+        {
+            radius = 0.0;
+        }
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public void GetRadius()
+        {
+            Console.WriteLine("Please enter the radius.");
+            radius = double.Parse(Console.ReadLine());
+        }
+
+        public void CalculateDiameter()
+        {
+            double diameter = 0;
+
+            diameter = 2 * radius;
+            Console.WriteLine("The diameter of the circle is" + diameter);
+        }
+        public void CalculateAreaCircle()
+        {
+            double area = 0;
+            double factor = 3.14;
+
+            area = Math.PI * radius * radius;
+            Console.WriteLine("The area of the circle is" + area);
+        }
+        public void Grow(double radius)
+        {
+            double grow = 0;
+            double factor = 3.14;
+            Console.WriteLine("The growth of the circle is" + grow ); 
+        }
+    }
+}
