06cc78bff500e2971ab7736e1219858f85d967e7
JazzyB06 <jasmine_boykins@yahoo.com>
2024-06-07 20:35:20 -0400

    Commit

---
+++ CarLotLab/CarLotLab/Car.cs
@@ -0,0 +1,59 @@
+﻿
+using Cars;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Cars;
+public class Car
+{
+    public string make { get; set; }
+    public string model { get; set; }
+    public int year { get; set; }
+    public decimal price { get; set; }
+
+    public Car()
+    {
+
+    }
+    public Car(string make, string model, int year, decimal price)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+    public override string ToString()
+    {
+        return ($"{year} {make} {model} ${price:N2} ");
+    }
+
+ 
+    public class UsedCar : Car
+    {
+        public double mileage { get; set; }
+        public UsedCar()
+        {
+        }
+        public UsedCar(string make, string model, int year, decimal price, double mileage)
+        : base(make, model, year, price)
+        {
+            this.mileage = mileage;
+        }
+        public override string ToString()
+        {
+            return ($"{year} {make} {model} ${price:N2} {mileage:N1} miles");
+        }
+        public static void Remove (int index)
+        {
+            if (index >= 0 && index < index)
+            {
+                Cars.Car Remove;
+                index--;
+            }
+            else
+            {
+                Console.WriteLine("The index is invalid. As a result, no car was removed.");
+            }
+        }
+    }
+}
---
+++ CarLotLab/CarLotLab/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
+using static Cars.Car;
+
+namespace Cars
+{
+    public class CarLab
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>
+            {
+                new Car("Hyundai", "Palisade", 2024, 38000),
+                new Car("Kia", "Sorento", 2024, 33000),
+                new Car("Ford", "Edge", 2024, 39000),
+                new UsedCar("Chevrolet", "Bolt EV", 2020, 16000, 38000),
+                new UsedCar("Mercedes-Benz", "C 300", 2020, 28000, 58000),
+                new UsedCar("BMW", "X1 XDrive28i", 2017, 21000, 35000),        
+            };
+            Console.WriteLine("Prior to removing:");
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.model);
+                //Car.remove(2);
+            }
+            Console.WriteLine("Welcome to GC's Car Emporium!");
+            Console.WriteLine("Here are the cars we have:");
+            ListCars(cars);
+            Console.WriteLine();
+            Console.WriteLine("Which car would you like? Kindly, enter your choice.");
+            string userinput = Console.ReadLine();
+            Console.WriteLine($"You selected: {userinput}");
+            Console.WriteLine("Great! Our finance department will be in touch shortly.");
+            Console.WriteLine("Have an amazing day!");
+
+            Console.WriteLine("Used Cars:");
+            foreach (Car car in cars)
+            {
+                if (car is UsedCar)
+                {
+                    Console.WriteLine(car.model + " " + car.make);
+                }
+            }
+
+            static void ListCars(List<Car> cars)
+            {
+                for (int i = 0; i < cars.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {cars[i]}");
+                }
+      
+            }
+        }
+
+        }
+    }
