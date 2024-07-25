e1e05318dae8244270d9b89ae677b66f1fbc3293
JazzyB06 <jasmine_boykins@yahoo.com>
2024-07-25 15:18:49 -0400

    Please push these changes to my website

---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShopRegistration.Models;
+
+namespace CoffeeShopRegistration.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    //The user on line 16 represent the data
+    //Post means from the browser or client to the controller
+    [HttpPost]
+    public IActionResult RegisterUser(User user)
+    {
+        return View();
+    }
+}
---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CoffeeShopRegistration.Models
+{
+	public class User
+	{
+		[Required(ErrorMessage = "First name is required")] //If not provided throw this error message
+		public string FirstName { get; set; }
+		[Required(ErrorMessage = "Last name is required")]
+		public string LastName { get; set; }
+		[EmailAddress] //check it to see the validation works
+		public string Email { get; set; }
+		[Required(ErrorMessage = "Password is required")]
+		[MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+		public string Password { get; set; }
+		//Validation is the process of checking what the user puts in to prevent them from having a choice to mess thing up
+	}
+}
