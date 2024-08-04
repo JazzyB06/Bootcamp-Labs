bd641c3234b01c19e1c2f9c65e1cc2bdc2e6f6a3
JazzyB06 <jasmine_boykins@yahoo.com>
2024-08-03 23:14:14 -0400

    Adoption MVC

---
+++ Adoption/Adoption/Controllers/AnimalController.cs
@@ -0,0 +1,58 @@
+﻿using Adoption.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adoption.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public AnimalController(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SearchAnimals(string breedInput)
+        {
+            //SELECT * FROM Animals
+            var animals = _dbContext.Animals.Where(x=>x.Breed == breedInput).ToList();
+            return View(animals);
+        }
+
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Animals.Add(animal);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(animal);
+        }
+        [HttpPost]
+        public IActionResult Adopt(int id)
+        {
+            var animal = _dbContext.Animals.Find(id);
+            if (animal != null)
+            {
+                _dbContext.Animals.Remove(animal);
+                _dbContext.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Adoption/Adoption/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace Adoption.Models.Data;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Image { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public int Age { get; set; }
+    public string Breed { get; set; }
+
+
+}
