416b4c931f7e22a80c29dabe9ba5e159460cf44a
JazzyB06 <jasmine_boykins@yahoo.com>
2024-08-06 18:13:26 -0400

    OMDB Lab

---
+++ MovieLab/MovieLab/Models/OMDBResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieLab.Models;
+
+public class OMDBResponse
+{
+    public List<Movie> Search { get; set; }
+    public string totalResults { get; set; }
+    public string Response { get; set; }
+}
---
+++ MovieLab/MovieLab/Services/OMDBService.cs
@@ -0,0 +1,33 @@
+﻿using MovieLab.Models;
+
+namespace MovieLab.Services
+{
+    public class OMDBService : IOMDBService
+    {
+        //make a method that call the api
+        //and returns the C# objects from the json
+        public async Task <OMDBResponse> SearchMovies(string movieName)
+        {
+            //create the url as a string with all the info in it
+            string url = $"http://www.omdbapi.com/?apikey=16efb775&s={movieName}";
+            //call the api and get a response
+            using (HttpClient client  = new HttpClient())
+            {
+                try
+                {
+                    OMDBResponse response = await client.GetFromJsonAsync<OMDBResponse>(url);
+
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return null;
+                }
+            }
+            //if the response is good, return the movies
+
+            //if the response is not good write a log
+        }
+    }
+}
